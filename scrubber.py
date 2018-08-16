c2e7045cc6f08a3388796b916a73e1168440fc41
teampsbox <teampsbox@gmail.com>
2018-08-16 19:12:38 +0800

    Changed names to exported and scrubbed

--- scrubber.py
+++ scrubber.py
@@ -1,9 +1,9 @@
 import csv
 
 fields_needed = ['email', 'Firstname', 'Lastname', 'DistributorStatus', 'SendersFirstName', 'MyEntireShakleeGroup', 'SendersShakleePWS', 'SendersYFPSitesURL', 'SendersFBGroup', 'SendersFanPage', 'SendersTwitter', 'SendersYoutube', 'SendersGooglePlus', 'SendersHeadShot', 'SendersYFP', 'SendersYWP']
-with open('export.csv', 'r') as fp_in, open('target.csv', 'w') as fp_out:
-    reader = csv.DictReader(fp_in)
-    writer = csv.DictWriter(fp_out, fieldnames=fields_needed, extrasaction='ignore')
+with open('export.csv', 'r') as exported, open('scrubbed.csv', 'w') as scrubbed:
+    reader = csv.DictReader(exported)
+    writer = csv.DictWriter(scrubbed, fieldnames=fields_needed, extrasaction='ignore')
 
     writer.writeheader()
     for r in reader:
